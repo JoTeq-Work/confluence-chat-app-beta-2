5720173cf8410ed73c6e3836b5a1f348a172a7ea
JoTeq-Work <joteqwork@gmail.com>
2023-11-29 12:33:47 +0000

    app updates

--- dependencies.py
+++ dependencies.py
@@ -1,40 +1,75 @@
 import os
 import json
+import logging
 import requests
+from pathlib import Path
+from openai import OpenAI
+from pydub.playback import play
 import speech_recognition as sr
+from playsound import playsound
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv, find_dotenv
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 GPT_MODEL = "gpt-3.5-turbo-0613"
 
 _ = load_dotenv(find_dotenv()) # read local .env
-openai_key = os.environ["OPENAI_API_KEY"]
 
+# Declare API keys
+openai_key = os.environ["OPENAI_API_KEY"]
+ATLASSIAN_API_TOKEN = os.environ["ATLASSIAN_API_TOKEN"]
+
+
+client = OpenAI(
+    api_key=openai_key
+)
 
 # Confluence REST API Utilities
-CONFLUENCE_SITE = "https://<your_site>.atlassian.net"
-API_TOKEN = "<ATLASSIAN_API_TOKEN>"
-AUTH = HTTPBasicAuth("<confluence_email>", API_TOKEN)
+CONFLUENCE_SITE = "https://joteqwork.atlassian.net"
+API_TOKEN = ATLASSIAN_API_TOKEN
+AUTH = HTTPBasicAuth("joteqwork@gmail.com", API_TOKEN)
 
 
-r = sr.Recognizer()
 def speech_to_text():   
-    """This function converts speech to text"""
+    r = sr.Recognizer() 
     with sr.Microphone() as source:
-        print("Say something")
-        audio_input = r.listen(source)
-    
+        logger.info("Say something...")
+        audio = r.listen(source)
+        
     try:
-        text_input = r.recognize_whisper(audio_input, language="english")
-        print("Input:", text_input)
-        return text_input
+        text = r.recognize_whisper(audio, language="english")
+        logger.info(f"Recognized speech: {text}")
+        return text
     except sr.UnknownValueError:
-        return "Whisper could not understand audio"
+        logger.warning("Speech recognition could not understand audio")
+        return None
     except sr.RequestError as e:
-        return "Could not request results from Whisper"
+        logger.error("Could not request results from speech recognition API: %s", e)
+        return None
     
+
+def text_to_speech(text):
+    if not text:
+        return
+    
+    speech_file_path = "static/chat_app/files/ai_output.mp3"
+    # speech_file_path = Path("static/chat_app/files") / "ai_output.mp3"
+    try:
+        response = client.audio.speech.create(
+        model="tts-1",
+        voice="shimmer",
+        input=text
+        )
+        response.stream_to_file(speech_file_path)
+        # playsound(speech_file_path)
+       
+    except Exception as e:
+        logger.error("Error generating speech: %s", e)
+
 
 def call_create_space_api(space_name):
     """
@@ -73,11 +108,11 @@ def call_create_space_api(space_name):
         auth=AUTH
         )
     except Exception as e:
-        print(f"Confluence Create Page API request failed with status code {response.status_code}")
-        print(f"Response content: {response.content}")
+        logger.error("Error with calling Confluence Create Space API: %s", e)
+        # print(f"Confluence Create Page API request failed with status code {response.status_code}")
+        # print(f"Response content: {response.content}")
 
     return response
-
 
 
 def call_create_page_api(title, content, space_id):
@@ -106,8 +141,9 @@ def call_create_page_api(title, content, space_id):
         auth=AUTH
         )  
     except Exception as e:
-        print(f"Confluence Create Page API request failed with status code {response.status_code}")
-        print(f"Respnse content: {response.content}")
+        logger.error("Error with calling Confluence Create Page API: %s", e)
+        # print(f"Confluence Create Page API request failed with status code {response.status_code}")
+        # print(f"Respnse content: {response.content}")
 
     return response
 
@@ -121,17 +157,17 @@ def chat_completion_request(messages, functions=None, model=GPT_MODEL):
     json_data = {"model": model, "messages": messages}
     if functions is not None:
         json_data.update({"functions": functions})
-        print("chat completion request, will try now")
+        logger.info("chat completion request, will try now")
     try:
         response = requests.post(
             "https://api.openai.com/v1/chat/completions",
             headers=headers,
             json=json_data,
         )
-        print(response.json())
+        logger.info(response.json())
         return response
     except Exception as e:
-        return f"Unable to generate ChatCompletion response. Exception: {e}"
+        logger.error("Unable to generate ChatCompletion response. Exception: %s", e)
     
 class Conversation:
     def __init__(self):
@@ -144,15 +180,15 @@ class Conversation:
         
 def chat_completion_with_function_execution(messages, functions=[None]):
     """This function makes a ChatCompletion API call with the option of adding functions"""
-    print("Started process")
+    logger.info("Started process")
     response = chat_completion_request(messages, functions)
-    print("First response", response.json()["choices"][0])
+    logger.info("First response", response.json()["choices"][0])
     full_message = response.json()["choices"][0]
     if full_message["finish_reason"] == "function_call":
-        print("Calling function")
+        logger.info("Calling function")
         return call_confluence_rest_api_function(messages, full_message)
     else:
-        print("Function not called")
+        logger.warning("Function not called")
         return response.json()
 
 
@@ -180,18 +216,17 @@ def call_confluence_rest_api_function(messages, full_message):
             parsed_output = json.loads(
                 full_message["message"]["function_call"]["arguments"]
             )
-            print("call_confluence_rest_api_function parsed out:", parsed_output)
+            logger.info("call_confluence_rest_api_function parsed out:", parsed_output)
             results = call_create_space_api(parsed_output["space_name"])
             id = results.json()['id']
-            print("id", id)
-            
-            
+            logger.info("id", id)
             space.set_spade_id(id)
             space_id = space.get_space_id()
-            print("space id", space_id)           
+            logger.info("space id", space_id)           
             
         except Exception as e:
-            return f"Unable to generate ChatCompletion response. Exception: {e}"
+            logger.error("Space error. Unable to generate ChatCompletion response: %s", e)
+            # return f"Unable to generate ChatCompletion response. Exception: {e}"
           
         messages.append(
             {
@@ -204,20 +239,21 @@ def call_confluence_rest_api_function(messages, full_message):
             response = chat_completion_request(messages)
             return response.json()
         except Exception as e:
-            print("space error")
-            raise Exception("Function chat request failed")
+            logger.error("Function chat request failed: %s", e)
+            # raise Exception("Function chat request failed")
     elif full_message["message"]["function_call"]["name"] == "call_create_page_api":    
         try:
             parsed_output = json.loads(
             full_message["message"]["function_call"]["arguments"]
             )
-            print("parsed_output:", parsed_output)
+            logger.info("parsed_output:", parsed_output)
             space_id = space.get_space_id()
-            print(space_id)
+            logger.info(space_id)
             results = call_create_page_api(parsed_output["title"], parsed_output["content"], space_id)
         except Exception as e:
-            print("page error")
-            return f"Unable to generate ChatCompletion response. Exception: {e}"
+            logger.error("Page error. Unable to generate ChatCompletion response: %s", e)
+            # print("page error")
+            # return f"Unable to generate ChatCompletion response. Exception: {e}"
             
         messages.append(
             {
@@ -230,9 +266,11 @@ def call_confluence_rest_api_function(messages, full_message):
             response = chat_completion_request(messages)
             return response.json()
         except Exception as e:
-            raise Exception("Function chat request failed")
+            logger.error("Function chat request failed: %s", e)
+            # raise Exception("Function chat request failed")
     else:
-        raise Exception("Function does not exist and cannot be called")
+        logger.warning("Function does not exist and cannot be called: %s", e)
+        # raise Exception("Function does not exist and cannot be called")
 
 
 
--- routers/chat_app.py
+++ routers/chat_app.py
@@ -1,3 +1,5 @@
+import logging
+from pathlib import Path
 from typing import Annotated
 
 from fastapi import APIRouter, Request, Form
@@ -5,7 +7,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from confluence_functions import confluence_functions
-from dependencies import Conversation, chat_completion_with_function_execution, speech_to_text
+from dependencies import Conversation, chat_completion_with_function_execution, speech_to_text, text_to_speech
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix="/chat",
@@ -14,24 +20,18 @@ router = APIRouter(
 
 templates = Jinja2Templates(directory="templates")
 
-# confai_system_message = """
-#     You are a friendly AI Confluence Liaison, a helpful assistant that communicates with Confluence Rest API.\
-#     When a user request to create a space, you are to ask the user for a space name \
-#     Always thank the user after they have provided the space name. \
-#     Move on to the next step and ask the user if they would like to create a page in the space. \
-#     If the user says yes or request to create a page, ask the user to provide content for the page for the page to be created. \
-#     Thank the user for providing content for the page.
-#     """
 confai_system_message = """
 You are a friendly AI Confluence Liaison, a helpful assistant that helps users create spaces and pages. \
     Use the phrase "create space" as a trigger to ask the user for a space name to create a space. \
         Listen to the space name provided by the user and save it as `space_name`. \
-    Always thank the user after they have provided the space name. \
+        Always Say "TrackMatrix confirms: The new Confluence Space is now ready to use" after the space has been created. \
 
 Ask the user to create a page for the space. \
-    Use the phrase "create page" as a trigger to ask the user for the title of the page.\
-    DO NOT assume the content of the page. Ask the user for content for the page.
+    Use the phrase "create page" as a trigger to ask the user for the title of the page. \
+    DO NOT assume the content of the page. Ask the user for content for the page. \
+    Always Say "TrackMatrix confirms: The new Confluence Page is now ready to use" after the page has been created.
 """
+
 confai_conversation = Conversation()
 confai_conversation.add_message("system", confai_system_message)
 
@@ -47,10 +47,15 @@ async def confluence_chat(request: Request):
     chat_response = chat_completion_with_function_execution(
         confai_conversation.conversation_history, functions=confluence_functions
     )
-    print("Chat response:", chat_response)
+    logger.info("Chat response:", chat_response)
     assistant_message = chat_response["choices"][0]["message"]["content"]
+    text_to_speech(assistant_message)
+    speech_file_path = "../static/chat_app/files/ai_output.mp3"
     
-    return templates.TemplateResponse("chat.html", {"request": request, "assistant_message": assistant_message})
+    return templates.TemplateResponse(
+        "chat.html", 
+        {"request": request, "assistant_message": assistant_message, "speech_file_path": speech_file_path}
+        )
 
 
 # @router.post("/test")
